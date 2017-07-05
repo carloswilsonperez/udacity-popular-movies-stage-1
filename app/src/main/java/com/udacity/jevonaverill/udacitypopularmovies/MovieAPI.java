6e8cd062e68502a4a15fc30a1d5fb0857c93e24e
Jevon Averill <jevonaverill@Jevons-MacBook-Pro.local>
2017-07-05 23:56:31 +0700

    Initial Commit

---
+++ app/src/main/java/com/udacity/jevonaverill/udacitypopularmovies/MovieAPI.java
@@ -0,0 +1,14 @@
+package com.udacity.jevonaverill.udacitypopularmovies;
+
+/**
+ * Created by jevonaverill on 7/2/17.
+ */
+
+public class MovieAPI {
+
+    public static String API_URL = "http://api.themoviedb.org/3/movie";
+    public static String IMAGE_URL = "http://image.tmdb.org/t/p/";
+    public static String IMAGE_SIZE_185 = "w185";
+    public static String IMAGE_NOT_FOUND = "http://www.classicposters.com/images/nopicture.gif";
+
+}
